dacd50de6e3b25f3ef60b01517f5cdd9d6acc108
Marc Mills <mdmills1990@gmail.com>
2018-09-12 17:11:23 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,10 +9,7 @@ end
 
 def kind
   if @side_a <= 0 || @side_b <= 0 || @side_c <= 0 || @side_a + @side_b <= @side_c || @side_a + @side_c <= @side_b || @side_b + @side_c <= @side_a
-    begin
       raise TriangleError
-      rescue TriangleError => error
-    end
   elsif @side_a == @side_b && @side_a == @side_c
     :equilateral
   elsif @side_a == @side_b && @side_a != @side_c
