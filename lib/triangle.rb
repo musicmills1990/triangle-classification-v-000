72208d49b1682102a653cd94bc6229211ca88fa9
Marc Mills <mdmills1990@gmail.com>
2018-09-12 17:05:16 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,7 +7,12 @@ def initialize(side_a, side_b, side_c)
 end
 
 def kind
-  if @side_a == @side_b && @side_a == @side_c
+  if @side_a <= 0 || @side_b <= 0 || @side_c <= 0 || @side_a + @side_b <= @side_c || @side_a + @side_c <= @side_b || @side_b + @side_c <= @side_a
+  begin
+    raise TriangleError
+  rescue TriangleError => error
+  end
+elsif @side_a == @side_b && @side_a == @side_c
     :equilateral
   elsif @side_a == @side_b && @side_a != @side_c
     :isosceles
@@ -17,8 +22,7 @@ def kind
     :isosceles
   elsif @side_a != @side_b && @side_a != @side_c && @side_b != @side_c
     :scalene
-  elsif @side_a <= 0 || @side_b <= 0 || @side_c <= 0 || @side_a + @side_b <= @side_c || @side_a + @side_c <= @side_b || @side_b + @side_c <= @side_a
-    raise TriangleError
+
   end
 end
 
