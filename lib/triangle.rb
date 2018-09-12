20c052a523c3da8ed26f9d3e3b934ed6ad9180cd
Marc Mills <mdmills1990@gmail.com>
2018-09-12 17:01:20 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,7 +18,7 @@ def kind
   elsif @side_a != @side_b && @side_a != @side_c && @side_b != @side_c
     :scalene
   elsif @side_a <= 0 || @side_b <= 0 || @side_c <= 0 || @side_a + @side_b <= @side_c || @side_a + @side_c <= @side_b || @side_b + @side_c <= @side_a
-    raise :TriangleError
+    raise TriangleError
   end
 end
 
