b0f597406c3d7e233dbdc487408fb1ed95f6e9fc
Marc Mills <mdmills1990@gmail.com>
2018-09-12 16:49:32 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,11 +8,11 @@ end
 
 def kind
   if @side_a == @side_b && @side_a == @side_c
-    "quilateral"
+    :equilateral
   elsif @side_a == @side_b && @side_a != @side_c
-    "isosceles"
+    :isosceles
   elsif @side_a == @side_c && @side_a != @side_c
-    "isosceles"
+    :isosceles
   end
 end
 
