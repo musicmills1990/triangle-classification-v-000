dea09666823565371cabac8cda1f05849f8fe1eb
Marc Mills <mdmills1990@gmail.com>
2018-09-12 16:53:29 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,8 @@ def kind
     :isosceles
   elsif @side_b == @side_c && @side_b != @side_a
     :isosceles
-    
+  elsif @side_a != @side_b && @side_a != @side_c && @side_b != @side_c
+    :scalene
   end
 end
 
