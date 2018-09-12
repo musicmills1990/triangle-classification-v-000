9098f8c82207703574e9a8d16f98f351f1192e9d
Marc Mills <mdmills1990@gmail.com>
2018-09-12 16:48:55 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,7 +6,17 @@ def initialize(side_a, side_b, side_c)
   @side_c = side_c
 end
 
+def kind
+  if @side_a == @side_b && @side_a == @side_c
+    "quilateral"
+  elsif @side_a == @side_b && @side_a != @side_c
+    "isosceles"
+  elsif @side_a == @side_c && @side_a != @side_c
+    "isosceles"
+  end
+end
 
+    
 
 class TriangleError < StandardError
 end
